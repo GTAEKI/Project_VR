d16d687982ab4c3c05b21e7de839c76686be433e
jaehyeonn <a01049469494@gmail.com>
2023-10-12 17:46:48 +0900

    스크립트, 씬 추가
    Asset 하위에 김재현 폴더 추가, Player 스크립트, 씬 추가

---
+++ Assets/KJH/Scripts/KJHPlayer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KJHPlayer : MonoBehaviour
+{
+    public int money;
+    public int hp;
+    public int ad;
+
+}
