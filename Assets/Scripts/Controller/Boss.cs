e600ce52eb0349036496380360250edd05fb3160
MSKim0215 <rlaalstjqu05@gmail.com>
2023-10-13 11:33:07 +0900

    feat: add to IDLE, MOVE state
    1) IDLE 상태 기능 추가
    2) MOVE 상태 기능 추가

--- Assets/Scripts/Controller/Boss.cs
+++ Assets/Scripts/Controller/Boss.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Boss : Character
@@ -8,45 +9,47 @@ public class Boss : Character
     /// 보스 상태 new enum
     /// 김민섭_231013
     /// </summary>
-    public new enum  CharacterState
+    public enum  CurrentPattern
     {
-       IDLE, MOVE, METEOR, SUMMON, DIE
+       METEOR, SUMMON, NONE
     }
 
-    protected new CharacterState state;     // 보스의 현재 상태, 김민섭_231013
+    [Header("패턴 상태")]
+    [SerializeField] private CurrentPattern currPattern = CurrentPattern.NONE;     // 보스의 현재 상태, 김민섭_231013
+
+    private GameObject playerTarget;        // 공격 목표 캐릭터 , 김민섭_231013
 
     /// <summary>
     /// 보스 현재 상태 프로퍼티
     /// 김민섭_231013
     /// </summary>
-    public new CharacterState State
+    public CurrentPattern CurrPattern
     {
-        get => state;
+        get => currPattern;
         set
         {
-            state = value;
+            currPattern = value;
 
             // TODO: state마다 애니메이션 실행
-            switch(state)
+            switch(currPattern)
             {
-                case CharacterState.IDLE: break;
-                case CharacterState.MOVE: break;
-                case CharacterState.METEOR: break;
-                case CharacterState.SUMMON: break;
-                case CharacterState.DIE: break;
+                case CurrentPattern.METEOR: break;
+                case CurrentPattern.SUMMON: break;
             }
         }
     }
 
     protected override void Update()
     {
-        switch(State)
-        {
-            case CharacterState.IDLE: UpdateIdle(); break;
-            case CharacterState.MOVE: UpdateMove(); break;
-            case CharacterState.METEOR: UpdateMeteor(); break;
-            case CharacterState.SUMMON: UpdateSummon(); break;
-            case CharacterState.DIE: UpdateDie(); break;
+        base.Update();
+
+        if (State == CharacterState.SKILL)
+        {   // 스킬 시전 상태일 경우 실행
+            switch (CurrPattern)
+            {
+                case CurrentPattern.METEOR: UpdateMeteor(); break;
+                case CurrentPattern.SUMMON: UpdateSummon(); break;
+            }
         }
     }
 
@@ -56,7 +59,38 @@ public class Boss : Character
     /// </summary>
     protected override void UpdateIdle()
     {
+        if(playerTarget != null)
+        {   // 타겟이 있다면 움직임 시작
+            State = CharacterState.MOVE;
+            return;
+        }
 
+        if(playerTarget == null)
+        {   // 타겟이 없다면 탐색 시작
+            Search();
+        }
+    }
+
+    /// <summary>
+    /// 타겟을 탐색하는 함수
+    /// 김민섭_231013
+    /// </summary>
+    private void Search()
+    {
+        Collider[] findTarget = Physics.OverlapSphere(transform.position, 100f, LayerMask.GetMask("Player"));
+        if (findTarget.Length <= 0)
+        {
+            Debug.LogWarning("타겟{플레이어}를 찾을 수 없습니다.");
+            return;
+        }
+
+        // 타겟을 찾았으면 움직임 상태로 전환
+        playerTarget = findTarget.First().gameObject;
+        startPosition = transform.position;
+        endPosition = playerTarget.transform.position;
+        endPosition.y = 2.5f;       // 임시 코드 , 김민섭_231013
+        State = CharacterState.MOVE;
+        return;
     }
 
     /// <summary>
@@ -65,7 +99,22 @@ public class Boss : Character
     /// </summary>
     protected override void UpdateMove()
     {
+        if(playerTarget == null)
+        {
+            Debug.LogWarning("타겟{플레이어}를 찾을 수 없습니다.");
+            State = CharacterState.IDLE;
+            return;
+        }
 
+        // 타겟이 있다면 타겟을 향해 이동
+        currentTime += Time.deltaTime * movementSpeed;
+
+        if(currentTime >= lerpTime)
+        {
+            currentTime = lerpTime;
+        }
+
+        transform.position = Vector3.Lerp(startPosition, endPosition, currentTime / lerpTime);
     }
 
     /// <summary>
@@ -74,7 +123,7 @@ public class Boss : Character
     /// </summary>
     private void UpdateMeteor()
     {
-
+        Debug.Log("메테오 발동!");
     }
 
     /// <summary>
@@ -83,7 +132,7 @@ public class Boss : Character
     /// </summary>
     private void UpdateSummon()
     {
-
+        Debug.Log("쫄몹 소환함");
     }
 
     /// <summary>
@@ -92,6 +141,6 @@ public class Boss : Character
     /// </summary>
     protected override void UpdateDie()
     {
-
+        Debug.Log("죽음");
     }
 }
--- Assets/Scripts/Controller/Character.cs
+++ Assets/Scripts/Controller/Character.cs
@@ -10,10 +10,18 @@ public class Character : MonoBehaviour
     /// </summary>
     public enum CharacterState
     {
-        IDLE, MOVE, DIE
+        IDLE, MOVE, SKILL, DIE
     }
 
-    protected CharacterState state;     // 캐릭터의 현재 상태 , 김민섭_231013
+    [Header("캐릭터 상태")]
+    [SerializeField] protected CharacterState state;     // 캐릭터의 현재 상태 , 김민섭_231013
+
+    // 졸개, 보스 전용 변수 (나중에 수정 예정) , 김민섭_231013
+    protected float movementSpeed = 1f;       // 캐릭터 이동속도 스탯(임시), 김민섭_231013
+    protected float currentTime = 0f;         // 시작 시간 (임시), 김민섭_231013
+    protected float lerpTime = 600f;          // 도착 시간 (600초) (임시), 김민섭_231013
+    protected Vector3 startPosition;          // 움직임 시작 지점 , 김민섭_231013
+    protected Vector3 endPosition;            // 움직임 도착 지점, 김민섭_231013
 
     /// <summary>
     /// 캐릭터의 현재 상태 프로퍼티
