2adf0184cbbd953c99c18ed6efe9e6083ec7edb4
BaeKyungTaek <kyungtaek0204@gmail.com>
2023-10-23 16:19:30 +0900

    feat: Add Cinemachine
    시네머신 추가 및 배경 이미지 추가

---
+++ Assets/01.BKT/Scripts_BKT/CameraManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraManager
+{
+    private CinemachineVirtualCamera inCastleCamera;
+    private CinemachineVirtualCamera outCastleCamera;
+
+    public void Init()
+    {
+        inCastleCamera = GameObject.Find("InCastleCamera").GetComponent<CinemachineVirtualCamera>();
+        outCastleCamera = GameObject.Find("OutCastleCamera").GetComponent<CinemachineVirtualCamera>();
+    }
+
+    public void StartCameraSet()
+    {
+        inCastleCamera.gameObject.SetActive(false);
+    }
+}
